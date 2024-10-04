49e5cbb237cb3fdfa8b8c32470576aebf74723b9
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-10-03 22:54:43 -0500

    Hooking up contact form

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const http = require('http');
 const express = require('express');
 const path = require('path');
+// const bodyParser = require('body-parser');
+// const nodemailer = require('nodemailer');
 
 const app = express();
 
-app.use(express.json());
 app.use(express.static("views"));
+app.use(express.json());
 
 // default URL for website
-app.use('/', function(req,res){
+app.get('/', function(req,res){
     res.sendFile(path.join(__dirname+'/views/index.html'));
     //__dirname : It will resolve to your project folder.
   });
+
+app.post('/send-message', (req, res) => {
+  console.log(req.body);
+});
 
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
 server.listen(port);
-console.debug('Server listening on port ' + port);
+console.debug('look at that beautiful server on port ' + port + ' p.s. golbez i haven\'t forgot what you did');
--- views/js/main.js
+++ views/js/main.js
@@ -1,7 +1,32 @@
-const hamburgerDrawer = document.querySelector(".drawer")
+const contactForm = document.querySelector('#contact-form');
+let formName = document.getElementById('name');
+let formEmail = document.getElementById('email');
+let formMessage = document.getElementById('message');
 
-// When anywhere in the hamburger drawer is clicked, it unchecks the checkbox and closes the hamburger menu.
-hamburgerDrawer.addEventListener("click", (event) => {
-    document.getElementById('hamburger-toggle').checked=false;
-});
 
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let formData = {
+        name: formName.value,
+        email: formEmail.value,
+        message: formMessage.value
+    };
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/send-message');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.onload = function() {
+        console.log(xhr.responseText);
+        if(xhr.responseText == 'success') {
+            alert('Email sent');
+            formName.value = '';
+            formEmail.value = '';
+            formMessage.value = '';
+        } else {
+            alert('Something went wrong');
+        }
+    }
+
+    xhr.send(JSON.stringify(formData));
+})
