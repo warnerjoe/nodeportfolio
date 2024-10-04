d593bd14abffacbf7dbaaddf9dc242849af31c36
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-10-04 00:05:36 -0500

    Connected email form

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 // const bodyParser = require('body-parser');
-// const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
+require('dotenv').config()
 
 const app = express();
 
@@ -17,6 +18,34 @@ app.get('/', function(req,res){
 
 app.post('/send-message', (req, res) => {
   console.log(req.body);
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.mail.me.com',
+    port: 587,
+    secure:false,
+    auth: {
+      user: process.env.ICLOUD_USER,
+      pass: process.env.ICLOUD_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.ICLOUD_USER,
+    to: 'warnerjoe337@icloud.com',
+    subject: 'Form submission',
+    text: req.body.message
+  }
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send('error');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.send('Success');
+    }
+  }
+  )
 });
 
 const server = http.createServer(app);
